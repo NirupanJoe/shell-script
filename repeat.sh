fe32592a53c33bff541d398ea5cea7f1b99a42e5
NirupanJoe <nirupanjoej@gmail.com>
2021-09-30 07:34:33 +0000

    Git status script.

--- repeat.sh
+++ repeat.sh
@@ -9,6 +9,16 @@
 
 # -----------------------------------------------------------
 
-# Print only the folder:
+# Print only the folder: sh repeat.sh
 
-echo $(ls -d */)
+# echo $(ls -d */)
+
+# Print the git status for the given path folder: sh repeat/repeat.sh
+
+for i in $(ls -d */)
+do
+    cd "$i"
+    echo "$i"
+    git status
+    cd ..
+done
