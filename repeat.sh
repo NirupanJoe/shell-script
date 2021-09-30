9b2ee66dff52934c685de68cebb838248895b6f9
NirupanJoe <nirupanjoej@gmail.com>
2021-09-30 07:31:34 +0000

    Print only the folder.

--- repeat.sh
+++ repeat.sh
@@ -2,9 +2,13 @@
 
 # Print the given Arguments: bash repeat.sh
 
-for ((i = 0 ; i < $2 ; i++));
-do
-  echo $1
-done
+# for ((i = 0 ; i < $2 ; i++));
+# do
+#   echo $1
+# done
 
 # -----------------------------------------------------------
+
+# Print only the folder:
+
+echo $(ls -d */)
